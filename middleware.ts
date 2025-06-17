024cebced8645abf8c433955a55ac788c3c097f2
Mehmet Kırmızıkaya <mehmetkirmizikaya46@gmail.com>
2025-06-17 23:13:49 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+
 export function middleware(request: NextRequest) {
-  const device = request.headers.get("x-device") || "unknown";
-
+  const device = request.headers.get("x-device") ?? "unknown";
   const response = NextResponse.next();
-  response.headers.set("x-device", device); // SSR tarafına taşı
-
+  response.headers.set("x-device", device);
   return response;
 }
+
